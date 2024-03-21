b0cc72c67588e7375260674928b03f2796720279
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-21 15:17:35 +0000

    tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,10 +15,13 @@ echo "<html>
     Holberton School
   </body>
 </html>
-" | sudo tee /data/web_static/releases/test/index.html > dev/null
+" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
+
+dataloc='\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}'
+sudo sed -i "43i ${dataloc}" /etc/nginx/sites-available/default
 
 sudo service nginx restart
