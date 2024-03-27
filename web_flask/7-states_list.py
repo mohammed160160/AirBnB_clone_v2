a66530f8f033e3b742e66e36f02e51a34bc226cb
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-27 20:21:08 +0000

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,9 +16,8 @@ def close_session(exception=None):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = list(storage.all("State").values())
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
